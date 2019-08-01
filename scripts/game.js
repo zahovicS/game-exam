27ff3aa09e7900f6c5e12137827bc9dfe98be0ee
Huarseral <huarseral@hotmail.com>
2019-08-01 18:37:40 -0500

    arrow

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,17 @@
+//empezamos a configurar el enterno del juego
 let config=
 {
+	//escogemos el tipo de configuracion
 	type: Phaser.AUTO,
+	//un ancho
 	width: 800,
+	//un alto
 	height:600,
+	//le indicamos que usaremos fisicas de arcade
 	physics:
 	{
 		default: 'arcade',
+		//establecemos un gravedad para el escenario
 		arcade:
 		{
 			gravity:
@@ -15,6 +21,7 @@ let config=
 			debug: false
 		}
 	},
+	//establecemos las escenas
 	scene:
 	{
 		preload: preload,
@@ -22,30 +29,43 @@ let config=
 		update: update
 	}
 };
+//variable del jugador
 let player;
-let objetives;
+//variable del objetivo: gasolina
+let gasolina;
+//variable del enimgo(s)
 let enemies;
+//variable de las plataformas
 let platforms;
+//variable de los botones de direccion
 let cursors;
+//variable que contiene el numero de puntaje
 let score=0;
+//variable de fin del juego
 let gameOver=false;
+//vriable del texto que contiene el puntaje
 let scoreText;
+//vriable del texto que se muestra al perder
 let gameOverText;
-
+//le damos a la variable game la configuracion antes echa
 let game=new Phaser.Game(config);
-
+//creamos la funcion de precarga
 function preload(){
-	//precargamos las imagenes
+	//precargamos las imagenes o sprites que vamos a utilizar
+				   //una clave, ubicacion
 	this.load.image('bg','img/escenario2.png');
 	this.load.image('platforms','img/platforms.png');
 	this.load.image('base1','img/base1.png');
-	this.load.image('base2','img/base2.png');
+	this.load.image('base2','img/base2.png');			  //ancho del sprite,alto del sprite
 	this.load.spritesheet('alien','img/sprite-aliens.png',{frameWidth:32,frameHeight: 32});
 }
+//creamos la funcion de crear
 function create(){
 	//añadimos las imagenes
+	//     ubicacion x,y   , la clave
 	this.add.image(400,300,'bg');
-	//asociamos las plataformas a un grupo estatico con fisicas
+
+	//asociamos las plataformas a un grupo estatico con fisicas, tambien hay dinamicas
 	platforms=this.physics.add.staticGroup();
 	//plataforma incial, posicion casi al inferior
     platforms.create(131,550,'base1');
@@ -54,6 +74,7 @@ function create(){
     platforms.create(-200,260,'platforms').setScale(2).refreshBody();
     platforms.create(400,130,'platforms');
     platforms.create(800,300,'platforms');
+
     //configuracion del jugador
     player=this.physics.add.sprite(300,200,'alien');
     //Establecemos un rebote de 0.2 al jugador
@@ -61,6 +82,7 @@ function create(){
     //Establecemos que el jugador podra tocar las plataformas
     player.setCollideWorldBounds(true);
     //creamos las animaciones en este caso la izquierda
+
     this.anims.create({
     	//una clave o identificador para llamarlo
     	key: 'izquierda',
@@ -69,12 +91,14 @@ function create(){
     	frameRate: 5,
     	repeat: -1
     });
+
     //ferente
     this.anims.create({
     	key: 'frente',
     	frames: [{key: 'alien', frame: 3}],
     	frameRate: 20
     });
+
     //derecha
     this.anims.create({
     	key: 'derecha',
@@ -82,30 +106,40 @@ function create(){
     	frameRate: 5,
     	repeat: -1
     });
+    //llamada de los cursores del teclado hacia la funcion cursors
     cursors = this.input.keyboard.createCursorKeys();
+    //establecemos que el jugador podra colisionar con las plataformas
     this.physics.add.collider(player,platforms);
 }
+//creamos la funcion de actualizar
 function update(){
-	
+	//las condicionales
+	//verificamos si el gameOver esta en verdadero, sino no hacemos nada
 	if (gameOver)
 	{
 		return;
 	}
-
+	//verificamos que este presionado las teclas establecidas: izquierda
 	if (cursors.left.isDown)
 	{
+		/*añadimos una velocidad en x negativa
+		para asi el jugador se mueva a la izquierda*/
 		player.setVelocityX(-160);
+		//la animacion del jugador en este caso la posicion de izquierda
 		player.anims.play('izquierda',true);
 	}
+	//derecha
 	else if(cursors.right.isDown)
 	{
 		player.setVelocityX(160);
 		player.anims.play('derecha',true);
 	}
+	//frente
 	else{
 		player.setVelocityX(0);
 		player.anims.play('frente');
 	}
+	//condicionamos si el jugador esta tocando el suelo osea una plataforma mas la tecla arriba
 	if (cursors.up.isDown && player.body.touching.down)
 	{
 		player.setVelocityY(-330);
