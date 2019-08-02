c7f67864580503925661a817156dc65a2f0ecfd8
zahovicS <zv.elrevelde@gmail.com>
2019-08-02 14:34:53 -0500

    update-button

--- scripts/game.js
+++ scripts/game.js
@@ -50,14 +50,23 @@ let gameOverText;
 //le damos a la variable game la configuracion antes echa
 let game=new Phaser.Game(config);
 //creamos la funcion de precarga
+let upButton;
+let up=false;
+let leftButton;
+let left=false;
+let rightButton;
+let right=false;
 function preload(){
 	//precargamos las imagenes o sprites que vamos a utilizar
 				   //una clave, ubicacion
 	this.load.image('bg','img/escenario2.png');
-	this.load.image('platforms','img/platforms.png');
+	this.load.image('platforms','img/platforms-2.png');
 	this.load.image('base1','img/base1.png');
 	this.load.image('base2','img/base2.png');			  //ancho del sprite,alto del sprite
 	this.load.spritesheet('alien','img/sprite-aliens.png',{frameWidth:32,frameHeight: 32});
+	this.load.image('up-arrow','img/up-arrow.png');
+	this.load.image('left-arrow','img/left-arrow.png');
+	this.load.image('right-arrow','img/right-arrow.png');
 }
 //creamos la funcion de crear
 function create(){
@@ -110,7 +119,58 @@ function create(){
     cursors = this.input.keyboard.createCursorKeys();
     //establecemos que el jugador podra colisionar con las plataformas
     this.physics.add.collider(player,platforms);
+
+    leftButton=this.add.image(70,540,'left-arrow').setInteractive();
+    upButton=this.add.image(720,530,'up-arrow').setInteractive();
+    rightButton=this.add.image(200,540,'right-arrow').setInteractive();
+
+    leftButton.on('pointerdown',function(){
+    	left=true;
+    });
+    leftButton.on('pointerout',doStop);
+
+    rightButton.on('pointerdown',function(){
+    	right=true;
+    });
+    rightButton.on('pointerout',doStop);
+    upButton.on('pointerdown',doJump);
+
 }
+//funciones de movimiento
+function doGoLeft()
+{	
+	/*añadimos una velocidad en x negativa
+	para asi el jugador se mueva a la izquierda*/
+	player.setVelocityX(-160);
+	//la animacion del jugador en este caso la posicion de izquierda
+	player.anims.play('izquierda', true);
+}
+
+function doGoRight()
+{
+	player.setVelocityX(160);
+	player.anims.play('derecha', true);
+}
+
+function doStop()
+{
+	player.setVelocityX(0);
+	player.anims.play('frente');
+	left=false;
+	right=false;
+}
+
+function doJump()
+{
+		//condicionamos si el jugador esta tocando el suelo
+	if (player.body.touching.down){
+		player.setVelocityY(-330);
+	}
+	else if(player.body.touching.down&&left||player.body.touching.down&&right){
+		player.setVelocityY(-330);
+	}
+}
+
 //creamos la funcion de actualizar
 function update(){
 	//las condicionales
@@ -120,28 +180,22 @@ function update(){
 		return;
 	}
 	//verificamos que este presionado las teclas establecidas: izquierda
-	if (cursors.left.isDown)
+	if (cursors.left.isDown||left)
 	{
-		/*añadimos una velocidad en x negativa
-		para asi el jugador se mueva a la izquierda*/
-		player.setVelocityX(-160);
-		//la animacion del jugador en este caso la posicion de izquierda
-		player.anims.play('izquierda',true);
+		doGoLeft();
 	}
 	//derecha
-	else if(cursors.right.isDown)
+	else if(cursors.right.isDown||right)
 	{
-		player.setVelocityX(160);
-		player.anims.play('derecha',true);
+		doGoRight();
 	}
 	//frente
 	else{
-		player.setVelocityX(0);
-		player.anims.play('frente');
+		doStop();
 	}
-	//condicionamos si el jugador esta tocando el suelo osea una plataforma mas la tecla arriba
-	if (cursors.up.isDown && player.body.touching.down)
+	//condicionamos si el jugador esta presionando la tecla arriba
+	if (cursors.up.isDown)
 	{
-		player.setVelocityY(-330);
+		doJump();
 	}
 }
